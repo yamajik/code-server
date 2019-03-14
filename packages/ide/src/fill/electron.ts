de2d496323fe2f794bcf21aa4998262c83863183
majik <me@yamajik.com>
2019-03-14 17:39:46 +0800

    fix resource base url

--- packages/ide/src/fill/electron.ts
+++ packages/ide/src/fill/electron.ts
@@ -46,7 +46,7 @@ const newCreateElement = <K extends keyof HTMLElementTagNameMap>(tagName: K): HT
 				return oldSrc!.get!.call(img);
 			},
 			set: (value: string): void => {
-				value = value.replace(/file:\/\//g, "/resource");
+				value = value.replace(/file:\/\//g, location.pathname.replace(/\/$/,'') + "resource");
 				oldSrc!.set!.call(img, value);
 			},
 		});
@@ -65,7 +65,7 @@ const newCreateElement = <K extends keyof HTMLElementTagNameMap>(tagName: K): HT
 				return oldInnerHtml!.get!.call(style);
 			},
 			set: (value: string): void => {
-				value = value.replace(/file:\/\//g, "/resource");
+				value = value.replace(/file:\/\//g, location.pathname.replace(/\/$/,'') + "resource");
 				oldInnerHtml!.set!.call(style, value);
 			},
 		});
@@ -78,7 +78,7 @@ const newCreateElement = <K extends keyof HTMLElementTagNameMap>(tagName: K): HT
 				if (sheet && !overridden) {
 					const oldInsertRule = sheet.insertRule;
 					sheet.insertRule = (rule: string, index?: number): void => {
-						rule = rule.replace(/file:\/\//g, "/resource");
+						rule = rule.replace(/file:\/\//g, location.pathname.replace(/\/$/,'') + "resource");
 						oldInsertRule.call(sheet, rule, index);
 					};
 					overridden = true;
@@ -139,8 +139,9 @@ const newCreateElement = <K extends keyof HTMLElementTagNameMap>(tagName: K): HT
 		(view as any).send = (channel: string, ...args: any[]): void => { // tslint:disable-line no-any
 			if (args[0] && typeof args[0] === "object" && args[0].contents) {
 				// TODO
-				args[0].contents = (args[0].contents as string).replace(/"(file:\/\/[^"]*)"/g, (m1) => `"/resource${m1}"`);
-				args[0].contents = (args[0].contents as string).replace(/"vscode-resource:([^"]*)"/g, (m, m1) => `"/resource${m1}"`);
+				const resourceBaseUrl = location.pathname.replace(/\/$/,'') + "resource";
+				args[0].contents = (args[0].contents as string).replace(/"(file:\/\/[^"]*)"/g, (m1) => `"${resourceBaseUrl}${m1}"`);
+				args[0].contents = (args[0].contents as string).replace(/"vscode-resource:([^"]*)"/g, (m, m1) => `"${resourceBaseUrl}${m1}"`);
 				args[0].contents = (args[0].contents as string).replace(/style-src vscode-core-resource:/g, "style-src 'self'");
 			}
 			if (view.contentWindow) {
