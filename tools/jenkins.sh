dee4f01a51fc3232486f6f89f6a14ac14b8f379c
majik <me@yamajik.com>
2020-01-09 11:45:16 +0800

    update

--- tools/jenkins.sh
+++ tools/jenkins.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Jenkins build started..."
-curl -X GET http://47.102.131.179:9090/job/code-server/build?token=code-server
+curl -X GET http://47.102.131.179:9090/job/Code-Server/build?token=Code-Server
